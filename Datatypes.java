ddb09889d65a25481d3e27cafae7204656aa3ade
LollatiSindhuja <sindhujalollati@gmail.com>
2025-01-26 18:23:56 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,23 @@
+public class Datatypes {
+    public static void main(String[] args){
+
+        int intvalue=10;
+        long longvalue=100000l;
+        byte bytevalue=100;
+        short shortvalue=1200;
+        char charvalue='A';
+        boolean booleanvalue=true;
+
+        System.out.println("Int value:"+intvalue);
+        System.out.println("Long value:"+longvalue);
+        System.out.println("Byte value:"+bytevalue);
+        System.out.println("Short value"+shortvalue);
+        System.out.println("Character value:"+charvalue);
+        System.out.println("Boolean value:"+booleanvalue);
+
+       
+
+
+    }
+    
+}
